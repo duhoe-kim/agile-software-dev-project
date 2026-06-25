0541b5430b498400d3ab8097049314e3ac244bde
duhoe-kim <duhoekim99@gmail.com>
2026-06-25 17:20:34 +0900

    Update __init__.py
    remove keys

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, abort
 from flask_session import Session
 
 from pymongo.mongo_client import MongoClient
@@ -6,13 +6,13 @@ from pymongo.server_api import ServerApi
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "44ec20ec802f8d0a566cacc378c624aaab60f14d06cf7d2137d1e52efd732ca4"
+app.config['SECRET_KEY'] = "<key>"
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-uri = "mongodb+srv://kim169:Iwpqo4nbk7LsWaW0@test.qfa1amv.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://kim169_db_user:<key>@dietsprintdb.uw39zbi.mongodb.net/"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
@@ -20,8 +20,9 @@ try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
-    print(e)
+    abort(408)
 
+from application.routes import error_handlers
 from application.routes import about
 from application.routes import main
 from application.routes import register
