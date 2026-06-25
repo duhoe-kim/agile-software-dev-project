1f3b5367581e0386a5edc15dcab4358db54003ba
duhoe-kim <duhoekim99@gmail.com>
2026-06-25 18:32:32 +0900

    Update validation.py
    add comments and unified coding standard

--- application/logics/validation.py
+++ application/logics/validation.py
@@ -3,15 +3,18 @@ from application.models.mongoDB.get import *
 
 import bcrypt
 
+#class used to through error message
 class CustomErrorMessage(Exception):
     pass
 
-def validate_primary_goal(BMI, primary_goal):
-    if primary_goal == "lose" and (BMI - 2) < 19:
+#if user goal would lead to underweight or overweight => throw error
+def validate_primary_goal(bmi, primary_goal):
+    if primary_goal == "lose" and (bmi - 2) < 19:
         raise CustomErrorMessage("Your goal would lead to underweight")
-    if primary_goal == "gain" and (BMI + 2) >= 24.9:
+    if primary_goal == "gain" and (bmi + 2) >= 24.9:
          raise CustomErrorMessage("Your goal would lead to obesity")
 
+#if user has set goal but setting target weight that does not suit the goal set => throw error
 def validate_target_weight(current_weight, target_weight, primary_goal):
     if primary_goal == "lose" and current_weight < target_weight:
         raise CustomErrorMessage("You will gain weight instead!")
@@ -20,32 +23,37 @@ def validate_target_weight(current_weight, target_weight, primary_goal):
     elif current_weight == target_weight:
         raise CustomErrorMessage("You will maintain weight instead!")
 
-def validate_target_BMI(target_weight, current_height):
-    target_BMI = calculate_BMI(target_weight, current_height)
+#if the target weight leads to underewight or overweight => throw error
+def validate_target_bmi(target_weight, current_height):
+    target_bmi = calculate_bmi(target_weight, current_height)
 
-    if target_BMI <= 19:
+    if target_bmi <= 19:
         raise CustomErrorMessage("Your target weight would lead to underweight")
-    elif target_BMI >= 30:
+    elif target_bmi >= 30:
         raise CustomErrorMessage("Your target weight would lead to obesity")
 
+#if the target require more changes than 20% of TDEE => throw error
 def validate_target(adc, daily_calories):
     acceptable_range = daily_calories/5
 
     if acceptable_range < adc:
-        raise CustomErrorMessage("Current target would cause damge to your health")
+        raise CustomErrorMessage("Current target would cause damage to your health")
 
+#if same as old password => throw error
 def validate_new_password(username, new_password):
     old_password_hashed = get_password(username)
     new_password_hashed = bcrypt.hashpw(new_password.encode("utf-8"), old_password_hashed)
 
     if old_password_hashed == new_password_hashed:
         raise CustomErrorMessage("New password is same as the old password, Please use other password")
-    
-def validate_new_current_weight(old_current_weight, new_current_weight):
-    if abs(old_current_weight - new_current_weight) > 5:
-        raise CustomErrorMessage("Plesae enter valid weight for your new current weight")
 
-def validate_new_current_weight(old_current_height, new_current_height):
+#if new weight has more than 5kg difference than the old weight => throw error
+def validate_new_current_weight(old_current_weight, new_current_weight):
+    if abs(old_current_weight - new_current_weight) > 10:
+        raise CustomErrorMessage("Plesae enter valid weight for your new current weight")
+        
+#if new height has more than 5cm difference than the old height => throw error
+def validate_new_current_height(old_current_height, new_current_height):
     if abs(old_current_height - new_current_height) > 5:
-        raise CustomErrorMessage("Plesae enter valid weight for your new current height")
+        raise CustomErrorMessage("Plesae enter valid height for your new current height")
     
